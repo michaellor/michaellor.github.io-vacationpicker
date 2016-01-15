1db513124bf762f242e112252297104cb2668b59
michaellor <student@epicodus.com>
2016-01-15 08:51:52 -0800

    add checkboxes

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,29 @@
 $(document).ready(function() {
+  // -- Function to have single checkbox click only.
+  var checkID;
+
+  function toggle(checkbox) {
+    if (checkbox.checked) {
+      if (checkID) {
+        document.getElementById(checkID).checked = false;
+      }
+      checkID = checkbox.getAttribute('id');
+    }
+  }
 
 
+
+  // $("input:checkbox").click(function() {
+  //     if ($(this).is(":checked")) {
+  //         var group = "input:checkbox[name='" + $(this).attr("name") + "']";
+  //         $(group).prop("checked", false);
+  //         $(this).prop("checked", true);
+  //     } else {
+  //         $(this).prop("checked", false);
+  //     }
+
+    event.preventDefault();
+  });
 
 
 });
