7c4af39b8feedca9bf0d363c99af9d2a2b3bffc7
michaellor <student@epicodus.com>
2016-01-15 12:08:18 -0800

    creat alternate html files

--- backupjs.js
+++ backupjs.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
 
   $("form#survey").submit(function(event) {
-  var sum1 = $("input[name=radio1]:checked").val();
-  var sum1 = $("input[name=radio1]:checked").val();
 
+    var sum1 = $("select#q1").val();
+    var sum2 = $("select#q2").val();
+    var sum3 = $("select#q3").val();
+    var sum4 = $("select#q4").val();
+    var sum5 = $("select#q5").val();
 
-
-
-  console.log(question1);
+    console.log(sum1);
 
   // if (total <= 0 && total >=3); {
   //   $("place1").show()}
