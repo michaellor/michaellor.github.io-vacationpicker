f20e6273544b2717b7e1ff702aadf45aac6972fb
michaellor <student@epicodus.com>
2016-01-15 10:30:00 -0800

    add row and columns

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,5 @@
 $(document).ready(function() {
 
-  $(':checkbox').bind('change', function() {
-          var thisClass = $(this).attr('class');
-          if ($(this).attr('checked')) {
-              $(':checkbox.' + thisClass + ":not(#" + this.id + ")").removeAttr('checked');
-          }
-          else {
-              $(this).attr('checked', 'checked');
-          }
-      });
 
-// function addClassCheck(el) {
-//
-//
-//   if(el.checked) {
-//     el.classList.add("checked");}
-//   else {
-//     el.classList.remove("checked");
-//     }
-//   }
-//
-//   if(document.getElementsByClassName("checked".length>1) {
-//     el.checked = false
-//     el.classList.remove("checked");
-//   }
 
 });
